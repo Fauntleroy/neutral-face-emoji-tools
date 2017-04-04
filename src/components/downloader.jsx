1b70937f4d519b691367f7609c3c0badd97dbc7c
Jared Stevick <jstevick@gmail.com>
2017-04-04 15:24:09 -0400

    download zip working

--- src/components/downloader.jsx
+++ src/components/downloader.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import cx from 'classnames';
 import findIndex from 'lodash/array/findIndex';
 import forEach from 'lodash/collection/forEach';
-import axios from 'axios';
+import map from 'lodash/collection/map';
+import last from 'lodash/array/last';
+import JSZip from 'jszip';
+import fileSaver from 'file-saver';
 
 class Download extends React.Component {
   constructor() {
@@ -10,6 +13,7 @@ class Download extends React.Component {
 
     this.state = {
       emojis: null,
+      loading: true,
     }
   }
   componentDidMount() {
@@ -21,52 +25,85 @@ class Download extends React.Component {
 
     forEach(rows, (s) => {
       let trimmedName = /^:([a-z0-9\-\_]+):/i.exec(s.innerText);
-      let trimmedUrl = /(https?:\/\/.*\.(?:png|jpg|gif))/.exec(s.cells[0].children[0].outerHTML);
+      let trimmedUrl = /(https?:\/\/.*\.(png|jpg|gif))/.exec(s.cells[0].children[0].outerHTML);
 
       emojis.push(
         {
           name: trimmedName[1] ? trimmedName[1] : s.innerText,
           url: trimmedUrl[1] ? trimmedUrl[1] : s,
+          ext: '.'+last(trimmedUrl),
         });
     });
 
-    this.setState({ emojis: emojis });
+    this.setState({
+      emojis: emojis,
+      loading: false,
+    });
   }
-  downloadEmojis(e) {
+  downloadEmojis() {
     const { emojis } = this.state;
 
-    e.preventDefault();
-    forEach(emojis, (e) => {
-      let x = new XMLHttpRequest();
-      x.open('GET', e.url);
-      x.responseType = 'blob';
-      x.onload = function() {
-          let blob = x.response;
-          let fr = new FileReader();
-          fr.onloadend = function() {
-            let base64 = fr.result;
-            let link = document.createElement("a");
-            link.setAttribute("href", base64);
-            link.setAttribute("download", e.name);
-            link.click();
+    this.setState({ loading: true });
+    // for each emoji, send a request
+    // anytime the request comes back, add that file's data to zip
+    // when the last one is done, download the zip
+
+    Promise.all(
+      map(emojis, (e) => {
+        return new Promise((resolve, _reject) => {
+          const x = new XMLHttpRequest();
+          x.open('GET', e.url);
+          x.responseType = 'blob';
+          x.onload = () => {
+            const blob = x.response;
+            const fr = new FileReader();
+            fr.onloadend = () => {
+              const buffer = fr.result;
+              resolve({ name: e.name + e.ext , buffer });
+            };
+            fr.readAsArrayBuffer(blob);
           };
-          fr.readAsDataURL(blob);
-      };
-      x.send();
+          x.send();
+        });
+      })
+    )
+    .then((buffers) => {
+      const zip = new JSZip();
+      forEach(buffers, ({ name, buffer }) => {
+        zip.file(name, buffer);
+      });
+      return zip.generateAsync({type:"blob"});
     })
-
-
+    .then((content) => {
+      fileSaver.saveAs(content, "emojis.zip");
+      this.setState({ loading: false });
+    });
   }
   render() {
-    const { emojis } = this.state;
+    const {
+      emojis,
+      loading,
+    } = this.state;
+
     return (
       <div className="slack-emoji-tools">
-        <h4 className="set__uploader__heading">Bulk Emoji Downloader</h4>
-        <button
-          onClick={(e) => this.downloadEmojis(e)}
-        >
-          Click here to download emojis
-        </button>
+        <h4>Bulk Emoji Downloader</h4>
+        <p>
+          Generate and download a zip file containing all {emojis.length} of your emojis for this Slack group.
+        </p>
+        {!loading ? (
+          <div>
+            <a
+              className="btn btn_primary"
+              onClick={() => this.downloadEmojis()}
+            >
+              Download Emojis
+            </a>
+          </div>
+          ) : (
+            <p>Loading...</p>
+          )
+        }
       </div>
     );
   }
