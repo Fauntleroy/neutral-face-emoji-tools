69636fdaa92b2e107a7b09f22a891a3245c74800
Cody Swartz <ctsstc@gmail.com>
2019-03-30 01:03:45 -0700

    Move functions below - stair stepping Thanks hoisting

--- src/content.js
+++ src/content.js
@@ -7,35 +7,6 @@ import './styles/content.less'; // Just to get it into the parcel build
 const ELEMENT_TO_INSERT_BEFORE_SELECTOR = '.p-customize_emoji_wrapper';
 const SET_ICON_URL = chrome.runtime.getURL('images/icon_128.png');
 
-let uploadElementId = 0;
-function createUploadElement (file) {
-  const filePreview = window.URL.createObjectURL(file);
-  const element = document.createElement('li');
-  element.id = `nfet__upload-${uploadElementId++}`;
-  element.classList.add('nfet__uploader__upload');
-  element.innerHTML = `
-    <img class="nfet__uploader__upload__preview" src="${filePreview}" />
-    <span class="nfet__uploader__upload__filename">${file.name}</span>
-    <span class="nfet__uploader__upload__status">
-      <i class="nfet__uploader__upload__status__icon nfet__uploader__upload__status__icon-uploading ts_icon ts_icon_spinner"></i>
-      <i class="nfet__uploader__upload__status__icon nfet__uploader__upload__status__icon-error ts_icon ts_icon_warning"></i>
-      <i class="nfet__uploader__upload__status__icon nfet__uploader__upload__status__icon-success ts_icon ts_icon_check_circle_o"></i>
-      <span class="nfet__uploader__upload__status__text"></span>
-    </span>`;
-
-  return element;
-}
-
-function addStyling() {
-  const styleUrl = chrome.runtime.getURL('content.css');
-  const link = document.createElement('link');
-  link.setAttribute('rel', 'StyleSheet');
-  link.setAttribute('href', styleUrl);
-  link.setAttribute('type', 'text/css');
-  link.setAttribute('id', 'dingus');
-  document.body.append(link);
-}
-
 elementReady(ELEMENT_TO_INSERT_BEFORE_SELECTOR).then(() => {
   addStyling();
   const elementToInsertBefore = document.querySelector(ELEMENT_TO_INSERT_BEFORE_SELECTOR);
@@ -89,3 +60,42 @@ elementReady(ELEMENT_TO_INSERT_BEFORE_SELECTOR).then(() => {
     q.start();
   });
 });
+
+function addStyling() {
+  const styleUrl = chrome.runtime.getURL('content.css');
+  const link = document.createElement('link');
+  link.setAttribute('rel', 'StyleSheet');
+  link.setAttribute('href', styleUrl);
+  link.setAttribute('type', 'text/css');
+  link.setAttribute('id', 'dingus');
+  document.body.append(link);
+}
+
+let uploadElementId = 0;
+function createUploadElement (file) {
+  const filePreview = window.URL.createObjectURL(file);
+  const element = document.createElement('li');
+  element.id = `nfet__upload-${uploadElementId++}`;
+  element.classList.add('nfet__uploader__upload');
+  element.innerHTML = `
+    <img class="nfet__uploader__upload__preview" src="${filePreview}" />
+    <span class="nfet__uploader__upload__filename">${file.name}</span>
+    <span class="nfet__uploader__upload__status">
+      <i class="nfet__uploader__upload__status__icon nfet__uploader__upload__status__icon-uploading ts_icon ts_icon_spinner"></i>
+      <i class="nfet__uploader__upload__status__icon nfet__uploader__upload__status__icon-error ts_icon ts_icon_warning"></i>
+      <i class="nfet__uploader__upload__status__icon nfet__uploader__upload__status__icon-success ts_icon ts_icon_check_circle_o"></i>
+      <span class="nfet__uploader__upload__status__text"></span>
+    </span>`;
+
+  return element;
+}
+
+function successfulUpload(element) {
+
+}
+
+function failedUpload(element, error) {
+  element.classList.add('nfet__uploader__upload--error');
+  element.querySelector('.nfet__uploader__upload__status__text').innerText = error;
+  console.error(error);
+}
