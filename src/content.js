35207bd307bbab4a10cfe39afe59974accbb527e
Cody Swartz <ctsstc@gmail.com>
2019-03-30 04:01:12 -0700

    Add error messages and fix styling id

--- src/content.js
+++ src/content.js
@@ -4,6 +4,7 @@ import queue from 'queue';
 import { SimpleDropzone } from 'simple-dropzone';
 import uploadEmoji from './upload-emoji';
 import './styles/content.less'; // Just to get it into the parcel build
+import getSlackErrorMessage from './slack-error-messages';
 
 const ELEMENT_TO_INSERT_BEFORE_SELECTOR = '.p-customize_emoji_wrapper';
 const SET_ICON_URL = chrome.runtime.getURL('images/icon_128.png');
@@ -72,7 +73,7 @@ function addStyling() {
   link.setAttribute('rel', 'StyleSheet');
   link.setAttribute('href', styleUrl);
   link.setAttribute('type', 'text/css');
-  link.setAttribute('id', 'dingus');
+  link.setAttribute('id', 'neutral-face-emoji-tools-styling');
   document.body.append(link);
 }
 
@@ -101,7 +102,8 @@ function successfulUpload(element) {
 }
 
 function failedUpload(element, error) {
+  const errorMessasge = getSlackErrorMessage(error);
   element.classList.add('nfet__uploader__upload--error');
-  element.querySelector('.nfet__uploader__upload__status__text').innerText = error;
+  element.querySelector('.nfet__uploader__upload__status__text').innerText = errorMessasge;
   console.log('Failed Upload', error);
 }
---
+++ src/slack-error-messages.js
@@ -0,0 +1,24 @@
+/** 
+ * These were manually pulled from the slack minified source
+ * They could change, and it's possible I may have missed something
+ *  with error_bad_name logic from the minified source.
+ * There could also be additional keys that may have been spread out
+ *  that I failed to catch as well.
+*/
+const tooMuch = "This image is a bit too big, even after resizing. A smaller file might help?";
+const badName = "Names must be lowercase, and can\u2019t contain spaces, periods, or most punctuation.";
+const errorMessages = {
+  "error_bad_upload": "Something happened that prevented your file from being uploaded. Try again?",
+  "error_bad_format": "Please make sure your file is a GIF, JPEG, or PNG.",
+  "resized_but_still_too_large": tooMuch,
+  "too_many_frames": tooMuch,
+  "error_too_big": "Please choose an image smaller than 64KB.",
+  "error_name_taken_i18n": "Unfortunately, the international emoji set already includes an emoji named <b>{newEmojiName}</b>. Mind trying a different name?",
+  "error_name_taken": "The name <b>{newEmojiName}</b> is already in use by another emoji.",
+  "error_bad_name": badName,
+  "error_bad_name_i18n": badName
+}
+
+export default function getSlackErrorMessage(key) {
+  return errorMessages[key] || key;
+}
